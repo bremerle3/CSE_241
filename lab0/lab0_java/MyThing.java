98fcaccce75e7875d39b152fd81b487411c329ab
lbremer@ACCOUNTS.AD.WUSTL.EDU <lbremer@ACCOUNTS.AD.WUSTL.EDU@4ce0e964-3df7-4bcc-8468-2583999c1fa8>
2015-01-26 03:16:58 +0000

    Fixed edge case errors.
    git-svn-id: https://shell.cec.wustl.edu:8443/cse241_sp15/svn/lbremer@20 4ce0e964-3df7-4bcc-8468-2583999c1fa8

--- lab0/lab0_java/MyThing.java
+++ lab0/lab0_java/MyThing.java
@@ -1,17 +1,18 @@
 public class MyThing {
-    
-    public void count(int x) 
-    {
-	// Print every odd number between 1 and x (inclusive) in the form
-	// 1...
-        // 3...
-	// <etc>
-	// x!
-    	for(int i = 1; i < x; i++){
-    		if(i % 2 == 1){
-    			System.out.println(i + "...");
-    		}
-    	}
-    	System.out.println(x + "!");
-    }
+
+	public void count(int x) {
+		// Print every odd number between 1 and x (inclusive) in the form
+		// 1...
+		// 3...
+		// <etc>
+		// x!
+		if (x > 0) {
+			for (int i = 1; i < x; i++) {
+				if (i % 2 == 1) {
+					System.out.println(i + "...");
+				}
+			}
+			if (x % 2 == 1) System.out.println(x + "!");
+		}
+	}
 }
